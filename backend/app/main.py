d9bbbf61aab0e83e5457a41e9f5b421a65e8284a
Michael Liu <liumichaeler@gmail.com>
2026-02-07 18:32:49 -0500

    remove localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173"],
+    allow_origins = ["https://arxtract-cxc.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
